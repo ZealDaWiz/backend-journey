0714cc99dd446bead2d45591fbf9ff04a9f23d10
ZealDaWiz <pelumioyatokun@gmail.com>
2026-03-18 09:56:40 +0100

    Changing Variables

---
+++ day02-conditions/start.js
@@ -0,0 +1,14 @@
+let name = "Zeal";
+let age = 23;
+
+console.log("Before");
+console.log("name");
+console.log("age");
+
+age = 24;
+name = "Zeal Oyatokun";
+
+console.log("After");
+console.log("name");
+console.log("age");
+
