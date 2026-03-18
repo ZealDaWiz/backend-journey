e50218bbb6d4c36bae1e71b63a917b49cae46b27
ZealDaWiz <pelumioyatokun@gmail.com>
2026-03-18 10:19:13 +0100

    Exam Grading System

--- day02-conditions/brain.js
+++ day02-conditions/brain.js
@@ -2,10 +2,9 @@ let score = 60;
 
 if (score >= 70) {
     console.log("Excellent");
+} else if (score >= 50) {
+         console.log("Pass");
+} else {
+    console.log("Fail");
 }
-else if (score >= 50) {
-    console.log("Passed");
-}
- else {
-    console.log("Failed");
-}
+
