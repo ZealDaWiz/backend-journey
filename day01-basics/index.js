301a85969163803d7920a3844415f7aa5211de54
ZealDaWiz <pelumioyatokun@gmail.com>
2026-03-18 08:50:48 +0100

    Updated Day 1 code

--- day01-basics/index.js
+++ day01-basics/index.js
@@ -1,3 +1,4 @@
 console.log("My nickname is Zeal");
 console.log("I am learning Backend Development");
 console.log("Today is my first day of programming");
+console.log("I will be committing my daily progress here on github");
