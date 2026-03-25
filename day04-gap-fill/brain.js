72b8e6dadd60c3234c06717fbb87e232c050d0d2
ZealDaWiz <pelumioyatokun@gmail.com>
2026-03-25 11:28:16 +0100

    Better Grading System

---
+++ day04-gap-fill/brain.js
@@ -0,0 +1,17 @@
+let score = 72;
+
+if (score >= 70) {
+   console.log("A - Excellent");
+}
+ else if (score >= 60) {
+       console.log("B - Good");
+}
+ else if (score >= 50) {
+      console.log("C - Pass");
+}
+ else if (score >= 40) {
+      console.log("D - Poor");
+}
+ else {
+      console.log("Fail");
+}
