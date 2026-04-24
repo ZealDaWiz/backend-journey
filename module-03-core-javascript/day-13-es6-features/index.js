ecb58ba42af8275a3f410c3a43e469c0b90bcc62
ZealDaWiz <pelumioyatokun@gmail.com>
2026-04-24 20:43:14 +0100

    Day 13: ES6 FEATURES

---
+++ module-03-core-javascript/day-13-es6-features/index.js
@@ -0,0 +1,58 @@
+// ===== ES6 FEATURES =====
+
+// ===== NORMAL FUNCTION =====
+function add(a, b) {
+     return a + b;
+}
+
+console.log(add(5, 3));
+
+// ===== ARROW FUNVTION - VERSION 1 =====
+const addition = (a, b) => {
+     return a + b;
+}
+
+console.log(addition(6, 2));
+
+// ===== ARROW FUNVTION - VERSION 2 =====
+const plus = (a, b) => a + b;
+
+console.log(plus(4,6));
+
+// ===== DESTRUCTURING =====
+let person = {
+    name: "Zeal",
+    age: 24,
+    city: "Abuja",
+};
+
+let {name, age, city} = person;
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+// ===== DESTRUCTURING ARRAYS =====
+let scores = [98,72, 55];
+
+let [first, second, third] = scores;
+
+console.log(first);
+console.log(second);
+console.log(third);
+
+// ===== SPREAD OPERATOR =====
+let firstBatch = ["Zeal", "Timi", "Dani"];
+let secondBatch = ["Kola", "Sade", "Bayo"];
+
+let allStudents = [...firstBatch, ...secondBatch];
+
+console.log(allStudents);
+
+// ===== DEFAULT PARAMETERS =====
+function greetUser(name = "Guest") {
+       console.log(`Welcome ${name}`);
+}
+
+greetUser("Zeal");
+greetUser();
