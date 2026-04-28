84b8ae427b4cf8e058c30f6754e5513d6ece0016
ZealDaWiz <pelumioyatokun@gmail.com>
2026-04-28 21:24:00 +0100

    Day 14: CLI mini project

---
+++ module-03-core-javascript/Day-14-cli-mini-project/brain.js
@@ -0,0 +1,10 @@
+// ===== Neimeth Pharmaceutical CLI System =====
+let medicines = [
+    {name: "Amlovar", price: 500, quantity: 2000, requiresPrescription: true},
+    {name: "Pancemol" price: 400, quantity: 1500, requiresPrescription: false},
+    {name: "Tiocosid", price: 2000, quantity: 800, requiresPrescription: true},
+    {name: "Hemafolin", price: 700, quantity: 1000, requiresPrescription: false},
+    {name: "Pyrantrin", price: 300, quantity: 5000, requiresPrescription: true},
+];
+
+
