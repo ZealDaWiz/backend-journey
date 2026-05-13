c6c7de64ce35ba37cba8c3b4a1ee2b9ad1069d97
ZealDaWiz <pelumioyatokun@gmail.com>
2026-05-13 21:23:46 +0100

    Updated Day 15: Module interaction

--- module-04-Node.js-Fundamentals/Day-15-How-Node.js-works/medicines.js
+++ module-04-Node.js-Fundamentals/Day-15-How-Node.js-works/medicines.js
@@ -1,2 +1,20 @@
-const medicines = ["Amlovar", "Pyrantrin", "Norduet", "Pancemol", "Unaben"];
+const os = require ('os');
+const medicines = [
+      {name: "Pancemol", price: 1000, quantity: 250, hasPrescription: false},
+      {name: "Pyrantrin", price: 500, quantity: 500, hasPrescription: true},
+      {name: "Norduet", price: 3000, quantity: 50, hasPrescription: true},
+      {name: "Amlovar", price: 800, quantity: 150, hasPrescription: false},
+      {name: "Unaben", price: 2500, quantity: 45, hasPrescription: true}
+];
 
+const findMedicine = (medicines, medicineName)  => {
+const found = medicines.filter((medicine) => {
+  return medicine.name === medicineName;
+});
+
+ if (found.length === 0) {
+  throw new Error("Medicine not found");
+ }
+}
+
+module.exports = {medicines, findMedicine, os};
