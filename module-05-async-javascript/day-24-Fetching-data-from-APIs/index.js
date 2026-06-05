c4d90dbadb64e9c8b8c28436e6c469632f1ed2f9
ZealDaWiz <pelumioyatokun@gmail.com>
2026-06-05 20:25:52 +0100

    Day 24: Fetching data from APIs

--- module-05-async-javascript/day-24-Fetching-data-from-APIs/index.js
+++ module-05-async-javascript/day-24-Fetching-data-from-APIs/index.js
@@ -1,3 +1,19 @@
 const fetchSimulator = (url) => {
    return new Promise((resolve, reject) => {
-     if 
+    setTimeout (() => {
+     console.log('run after 2 seconds');
+     resolve({url: url, data: "response from " + url, status: 200});
+    }, 2000);
+  });
+}
+
+async function showResult(url) {
+   try {
+       const result = await fetchSimulator(url);
+         console.log(result);
+   } catch(error) {
+         console.log('Error:', error.message);
+   }
+}
+
+showResult("https://api.example.com");
