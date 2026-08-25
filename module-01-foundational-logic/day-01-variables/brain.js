8e80ec074a413d9dafb80536bcc4d66b852ed693
ZealDaWiz <pelumioyatokun@gmail.com>
2026-08-25 06:25:48 +0100

    Brain.js code summarized

--- module-01-foundational-logic/day-01-variables/brain.js
+++ module-01-foundational-logic/day-01-variables/brain.js
@@ -7,3 +7,25 @@ let age = year - birthYear;
 console.log("Hello " + name);
 console.log("You are " + age + " years old");
 
+/* name is a variable that contains string "Zeal". so is year and birthyear.
+ To get the age, the birthYear is subtracted from the current year but this
+ "HARDCODED".
+Therefore, when you console.log, you use concatenate to join your intent with the
+variable containing string (value) e.g name contains "Zeal", as it us assigned to it
+by an assignment operator (=). 
+
+To make your code more dynamic, tou coukd write it using Javascript built-in
+Date object as shown below: */
+
+let currentYear = new Date().getFullYear();
+
+ age = currentYear - birthYear;
+
+/* Note: let is used in declaring a variable. the use of let was ignored when 
+ Reassinging the age box with dynamic calculation. */
+
+console.log("Your dynamic age is " + age + " years old");
+
+/* There is usually a space between your intent word and your variable when using
+ concatenation in your console.log. This is to ensure that there is adequate spacing
+ when your program is being run. */
