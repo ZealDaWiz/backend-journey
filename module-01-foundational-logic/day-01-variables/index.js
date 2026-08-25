d1bcf3cdf9d8cff78cc4e1f9fd8674c15fe8b304
ZealDaWiz <pelumioyatokun@gmail.com>
2026-08-25 05:44:38 +0100

    Day 1: Adding summary to code for easy readability

--- module-01-foundational-logic/day-01-variables/index.js
+++ module-01-foundational-logic/day-01-variables/index.js
@@ -1,4 +1,13 @@
-console.log("My nickname is Zeal");
+console.log("My name is Olalekan Boluwatife Oyatokun");
+console.log("My nickname is ZealdaWiz and I am 24 years old");
 console.log("I am learning Backend Development");
 console.log("Today is my first day of programming");
 console.log("I will be committing my daily progress here on github");
+
+/* In Basic Backend Development, one has to understand the use of "console.log".
+ console.log helps carry the information you intend to convey when your program is been
+ run.
+
+In Backend Development, Semi-colons (;) helps identify where a code stops, so that
+JavaScript do not mistake intent(i.e join codes together) and also, to enable easy
+readability of your code and track bugs in your code easily */
